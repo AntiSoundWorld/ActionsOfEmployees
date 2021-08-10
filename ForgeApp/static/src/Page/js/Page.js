8e8832afd6735579d2416c29af03919592c633c9
antisoundworld <antisoundworld@gmail.com>
2021-08-10 11:12:16 +0300

    add updateDatas button

--- ForgeApp/static/src/Page/js/Page.js
+++ ForgeApp/static/src/Page/js/Page.js
@@ -4,7 +4,7 @@ function Page(){
     return(
         // <iframe id="login" src="https://11d4e69cd526.ngrok.io" frameBorder="0"></iframe>
         <iframe id="login" src="https://actionofemployeesapp.herokuapp.com" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin
-        allow-scripts allow-forms"></iframe>
+        allow-scripts allow-forms allow-top-navigation"></iframe>
     )
 }
 
